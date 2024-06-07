778b72852bdd49f35fccf28f8e0725babced5a87
Nathan Davouse <nathan.davouse@gmail.com>
2024-06-07 17:19:13 +0200

    Update linear_model_1.py

--- python_model/linear_model_1.py
+++ python_model/linear_model_1.py
@@ -3,8 +3,6 @@ import numpy as np
 np.set_printoptions(suppress=True)
 import pandas as pd
 
-# Create a new model
-
 
 # Parameters
 I = 10  # number of consumers
@@ -12,19 +10,20 @@ J = 10  # number of bundles
 T = 6   # number of periods
 beta = 0.5  # decreasing rate for the creation of bundles
 b = 0.04    # deterioration rate over time
-M = 10 * sum([4, 8, 12, 16, 20, 24, 28, 32, 36, 40])  # Big M
+c = [4*(j+1) for j in range(J)]# Costs of bundles
+M = 10 * sum(c)  # Big M
 max_r = 12
 lamb = 0.5
 min_r = max_r*lamb
 
 
-# Costs of bundles
-c = [4*(j+1) for j in range(J)]
+rng = np.random.default_rng(78794)
+
 
 # Compute reservation prices
 def reservation_prices(I,J,T,beta,b,min_r,max_r):
     r = np.full((I,J,T),0.)
-    r[:,0,0] = np.random.uniform(min_r,max_r,I)
+    r[:,0,0] = rng.uniform(min_r,max_r,I)
     for i in range(I):
         for j in range(J):
             for t in range(T):
@@ -61,7 +60,8 @@ def linear_model(I,J,T,M,c,r):
     model.addConstrs((l[j, t] - M * y[j, t] <= 0 for j in range(J) for t in range(T)), name="Linearisation_Ljt_4")
     model.addConstrs((r[i][j][t] * x[i, j, t] - k[i, j, t] >= 0 for i in range(I) for j in range(J) for t in range(T)), name="Customer_choice")
     model.addConstrs((quicksum(x[i, j, t] for j in range(J) for t in range(T)) <= 1 for i in range(I)), name="Single_Purchase")
-    model.addConstrs((quicksum(y[j, t] for j in range(J)) <= 1 for t in range(T)), name="Single_Bundle")
+    model.addConstrs((quicksum(y[j, t] for j in range(J)) == 1 for t in range(T)), name="Single_Bundle")
+    #model.addConstrs((quicksum(y[j, t] for j in range(J)) <= 1 for t in range(T)), name="Single_Bundle")
     model.addConstrs((x[i, j, t] <= y[j, t] for i in range(I) for j in range(J) for t in range(T)), name="Production_Limit")
 
     # Solve the model
@@ -79,47 +79,52 @@ Export_df = pd.DataFrame(columns=columns)
 
 def new_line_df(beta = beta, b = b, lamb = lamb):
     #Table like article table
-    A = np.full((T,3),0.)
+    A = np.full((T,4),0.)
     A[:,0]=[t+1 for t in range(T)]
     for j in range(J):
         for t in range(T):
             if variables["y"][j,t].x == 1 :
-                A[t,1]=j
+                A[t,1]=j+1
                 A[t,2]=variables["p"][j,t].x
+                A[t,3]=A[t,2]/A[t,1]
+
     S = [variables["s"][i].x for i in range(I)]
     return [beta,b,S,A,model.ObjVal,lamb]
 
+Export_df.loc[0] = new_line_df()
+print(Export_df["Profit"])
+loo=False
+
+if loo:
+    beta_list = [0.2,0.5,0.8]
+    lambda_list = [0.2,0.5,0.8]
+    b_list = [0.01,0.04,0.07]
+
+    df_indice = 0
+
+    #Test multiple beta values
+    for temp_beta in beta_list:
+        r = reservation_prices(I,J,T,temp_beta,b,min_r,max_r)
+        model,variables = linear_model(I,J,T,M,c,r)
+        Export_df.loc[df_indice] = new_line_df(beta = temp_beta)
+        df_indice+=1
+
+    #Test multiple b values
+    for temp_b in b_list:
+        r = reservation_prices(I,J,T,beta,temp_b,min_r,max_r)
+        model,variables = linear_model(I,J,T,M,c,r)
+        Export_df.loc[df_indice] = new_line_df(b = temp_b)
+        df_indice+=1
 
 
-beta_list = [0.2,0.5,0.8]
-lambda_list = [0.2,0.5,0.8]
-b_list = [0.01,0.04,0.07]
+    #Test multiple lambda values
+    for temp_lamb in lambda_list:
+        temp_min_r = temp_lamb*max_r
+        r = reservation_prices(I,J,T,beta,b,temp_min_r,max_r)
+        model,variables = linear_model(I,J,T,M,c,r)
+        Export_df.loc[df_indice] = new_line_df(lamb=temp_lamb)
+        df_indice+=1
 
-df_indice = 0
-
-#Test multiple beta values
-for temp_beta in beta_list:
-    r = reservation_prices(I,J,T,temp_beta,b,min_r,max_r)
-    model,variables = linear_model(I,J,T,M,c,r)
-    Export_df.loc[df_indice] = new_line_df(beta = temp_beta)
-    df_indice+=1
-
-#Test multiple b values
-for temp_b in b_list:
-    r = reservation_prices(I,J,T,beta,temp_b,min_r,max_r)
-    model,variables = linear_model(I,J,T,M,c,r)
-    Export_df.loc[df_indice] = new_line_df(b = temp_b)
-    df_indice+=1
-
-
-#Test multiple lambda values
-for temp_lamb in lambda_list:
-    temp_min_r = temp_lamb*max_r
-    r = reservation_prices(I,J,T,beta,b,temp_min_r,max_r)
-    model,variables = linear_model(I,J,T,M,c,r)
-    Export_df.loc[df_indice] = new_line_df(lamb=temp_lamb)
-    df_indice+=1
-
-     
-Export_df.to_json("GP30_Results.json",orient="records",lines=True)
+        
+    Export_df.to_json("GP30_Results.json",orient="records",lines=True)
 

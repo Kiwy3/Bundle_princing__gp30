d8208c25a562c651e0eed98569c500ec57e143fb
Nathan Davouse <nathan.davouse@gmail.com>
2024-06-17 16:14:34 +0200

    add image, change lambda for bound,

--- python_model/bundle_model.py
+++ python_model/bundle_model.py
@@ -8,10 +8,13 @@ author : Nathan Davouse
 
 #Maximum willingness to pay
 def reservation_prices(I,J,T,beta,b,min_r,max_r,seed = 78794 ):
+    """----------- Import numpy librairy ----------"""
     import numpy as np
+    """----------- Define R1ii with uniform generation ----------"""
     rng = np.random.default_rng(seed)
     r = np.full((I,J,T),0.)
     r[:,0,0] = rng.uniform(min_r,max_r,I)
+    """----------- Compute Rijt ----------"""
     for i in range(I):
         for j in range(J):
             for t in range(T):
@@ -22,39 +25,83 @@ def reservation_prices(I,J,T,beta,b,min_r,max_r,seed = 78794 ):
 
 #Define the linear model and optimize it
 def linear_model(I,J,T,M,c,r):
+    """----------- Import function from gurobipy librairy ----------"""
     from gurobipy import Model
     from gurobipy import GRB
     from gurobipy import quicksum
 
+    """----------- Create Gurobipy model ----------"""
     model = Model("Linear_Problem_IP")
-    # Variables
+    
+    """----------- Add variables ----------"""
     p = model.addVars(J, T, name="p", lb=0)
     s = model.addVars(I, name="s", lb=0)
-    x = model.addVars(I, J, T, vtype=GRB.BINARY, name="x")
-    y = model.addVars(J, T, vtype=GRB.BINARY, name="y")
+    x = model.addVars(I, J, T, vtype=GRB.BINARY, name="x") #Include constraint 13
+    y = model.addVars(J, T, vtype=GRB.BINARY, name="y") #Include constraint 14
     l = model.addVars(J, T, name="l", lb=0)
     k = model.addVars(I, J, T, name="k", lb=0)
+    #Make a dictionnary with all variables to return it
     model_vars = {"p":p,"s":s,"x":x,"y":y,"l":l,"k":k}
-    # Objective function
+
+    """----------- Set Objective function ----------"""
     model.setObjective(
         quicksum(k[i, j, t] - c[j] * x[i, j, t] for i in range(I) for j in range(J) for t in range(T)),
         GRB.MAXIMIZE
     )
 
-    # Constraints
-    model.addConstrs((k[i, j, t] - M * (1 - x[i, j, t]) <= p[j, t] for i in range(I) for j in range(J) for t in range(T)), name="Linearisation_kijt_1")
-    model.addConstrs((k[i, j, t] + M * (1 - x[i, j, t]) >= p[j, t] for i in range(I) for j in range(J) for t in range(T)), name="Linearisation_kijt_2")
-    model.addConstrs((k[i, j, t] - M * x[i, j, t] <= 0 for i in range(I) for j in range(J) for t in range(T)), name="Linearisation_kijt_3")
-    model.addConstrs((s[i] == quicksum(r[i][j][t] * x[i, j, t] - k[i, j, t] for j in range(J) for t in range(T)) for i in range(I)), name="Linear_Si")
-    model.addConstrs((s[i] >= r[i][j][t] * y[j, t] - l[j, t] for i in range(I) for j in range(J) for t in range(T)), name="Linearisation_Ljt_1")
-    model.addConstrs((l[j, t] - M * (1 - y[j, t]) <= p[j, t] for j in range(J) for t in range(T)), name="Linearisation_Ljt_2")
-    model.addConstrs((l[j, t] + M * (1 - y[j, t]) >= p[j, t] for j in range(J) for t in range(T)), name="Linearisation_Ljt_3")
-    model.addConstrs((l[j, t] - M * y[j, t] <= 0 for j in range(J) for t in range(T)), name="Linearisation_Ljt_4")
-    model.addConstrs((r[i][j][t] * x[i, j, t] - k[i, j, t] >= 0 for i in range(I) for j in range(J) for t in range(T)), name="Customer_choice")
-    model.addConstrs((quicksum(x[i, j, t] for j in range(J) for t in range(T)) <= 1 for i in range(I)), name="Single_Purchase")
-    #model.addConstrs((quicksum(y[j, t] for j in range(J)) == 1 for t in range(T)), name="Single_Bundle")
-    model.addConstrs((quicksum(y[j, t] for j in range(J)) <= 1 for t in range(T)), name="Single_Bundle")
-    model.addConstrs((x[i, j, t] <= y[j, t] for i in range(I) for j in range(J) for t in range(T)), name="Production_Limit")
+    """----------- Add Constraints ----------"""
+    #Constraint 3.2
+    model.addConstrs((k[i, j, t] - M * (1 - x[i, j, t]) <= p[j, t] 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Linearisation_kijt_1")
+    #Constraint 3.3
+    model.addConstrs((k[i, j, t] + M * (1 - x[i, j, t]) >= p[j, t] 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Linearisation_kijt_2")
+    #Constraint 3.4
+    model.addConstrs((k[i, j, t] - M * x[i, j, t] <= 0 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Linearisation_kijt_3")
+    #Constraint 4.1
+    model.addConstrs((s[i] == quicksum(r[i][j][t] * x[i, j, t] - k[i, j, t] 
+                                       for j in range(J) for t in range(T)) for i in range(I)), 
+                                       name="Linear_Si")
+    #Constraint 5.1
+    model.addConstrs((s[i] >= r[i][j][t] * y[j, t] - l[j, t] 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Linearisation_Ljt_1")
+    #Constraint 5.2
+    model.addConstrs((l[j, t] - M * (1 - y[j, t]) <= p[j, t] 
+                      for j in range(J) for t in range(T)), 
+                      name="Linearisation_Ljt_2")
+    #Constraint 5.3
+    model.addConstrs((l[j, t] + M * (1 - y[j, t]) >= p[j, t] 
+                      for j in range(J) for t in range(T)), 
+                      name="Linearisation_Ljt_3")
+    #Constraint 5.4
+    model.addConstrs((l[j, t] - M * y[j, t] <= 0 
+                      for j in range(J) for t in range(T)), 
+                      name="Linearisation_Ljt_4")
+    #Constraint 6
+    model.addConstrs((r[i][j][t] * x[i, j, t] - k[i, j, t] >= 0 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Customer_choice")
+    #Constraint 8
+    model.addConstrs((quicksum(x[i, j, t] 
+                               for j in range(J) for t in range(T)) <= 1 for i in range(I)), 
+                               name="Single_Purchase")
+    #Constraint 9 : 
+    model.addConstrs((quicksum(y[j, t] for j in range(J)) <= 1 
+                      for t in range(T)), 
+                      name="Single_Bundle")
+    #Constraint 9 bis : Force retailer to propose one bundle
+    #model.addConstrs((quicksum(y[j, t] for j in range(J)) == 1 
+        # for t in range(T)), 
+        # name="Single_Bundle")
+    #Constraint 10
+    model.addConstrs((x[i, j, t] <= y[j, t] 
+                      for i in range(I) for j in range(J) for t in range(T)), 
+                      name="Production_Limit")
 
     # Solve the model
     model.optimize()
--- python_model/sensi_analysis.py
+++ python_model/sensi_analysis.py
@@ -9,6 +9,7 @@ author : Nathan Davouse
 import numpy as np
 np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
 #import bundle_model functions
 from bundle_model import reservation_prices 
 from bundle_model import linear_model
@@ -28,10 +29,12 @@ rdm_seed = 654654
 
 #Controls variables
 loo=True
+rand = False
 expo = True
+plot = False
 
 """-----------Make a list with keys results----------"""
-def new_line_df(beta = beta, b = b, lamb = lamb):
+def new_line_df(beta = beta, b = b, min_r=min_r, max_r=max_r):
     #Table like article table
     A = np.full((T,5),0.)
     A[:,0]=[t+1 for t in range(T)]
@@ -44,14 +47,14 @@ def new_line_df(beta = beta, b = b, lamb = lamb):
                 A[t,4] = sum([variables["x"][i,j,t].x for i in range (I)])
     S = [variables["s"][i].x for i in range(I)]
 
-    return [beta,b,lamb,S,A,model.ObjVal]
+    return [beta,b,min_r,max_r,S,A,model.ObjVal]
 
 """-----------optimize with initial results----------"""
-r = reservation_prices(I,J,T,beta,b,min_r,max_r,rdm_seed)
+r = reservation_prices(I,J,T,beta,b,min_r,max_r)
 model,variables = linear_model(I,J,T,M,c,r)
 
 """-----------Create Dataframe and save with initial results----------"""
-columns = ["beta","b","lambda","Si","Results","Profit"]
+columns = ["beta","b","min_r","max_r","Si","Results","Profit"]
 Export_df = pd.DataFrame(columns=columns)
 Export_df.loc[0] = new_line_df()
 print(Export_df["Profit"])
@@ -60,32 +63,47 @@ print(Export_df["Profit"])
 if loo:
     #Define list of values for sensibility analysis
     beta_list = [0.2,0.8] #remove 0.5 since it's initial value
-    lambda_list = [0.2,0.8] #remove 0.5 since it's initial value
     b_list = [0.01,0.07] #remove 0.04 since it's initial value
+    minR_list = [7,8] #remove 0.5 since it's initial value
+    maxR_list = [11,10] #remove 0.5 since it's initial value
 
     df_indice = 1
     #Test multiple beta values
     for temp_beta in beta_list:
-        r = reservation_prices(I,J,T,temp_beta,b,min_r,max_r,rdm_seed)
+        r = reservation_prices(I,J,T,temp_beta,b,min_r,max_r)
         model,variables = linear_model(I,J,T,M,c,r)
         Export_df.loc[df_indice] = new_line_df(beta = temp_beta)
         df_indice+=1
 
     #Test multiple b values
     for temp_b in b_list:
-        r = reservation_prices(I,J,T,beta,temp_b,min_r,max_r,rdm_seed)
+        r = reservation_prices(I,J,T,beta,temp_b,min_r,max_r )
         model,variables = linear_model(I,J,T,M,c,r)
         Export_df.loc[df_indice] = new_line_df(b = temp_b)
         df_indice+=1
 
 
     #Test multiple lambda values
-    for temp_lamb in lambda_list:
-        temp_min_r = temp_lamb*max_r
-        r = reservation_prices(I,J,T,beta,b,temp_min_r,max_r,rdm_seed)
+    for i in [0,1]:
+        r = reservation_prices(I,J,T,beta,b,minR_list[i],maxR_list[i] )
         model,variables = linear_model(I,J,T,M,c,r)
-        Export_df.loc[df_indice] = new_line_df(lamb=temp_lamb)
+        Export_df.loc[df_indice] = new_line_df(min_r=minR_list[i],max_r=maxR_list[i])
         df_indice+=1
 
+"""-----------Test initial parameters with multiple random seeds----------"""
+if rand : 
+    n_val = 100
+    seeds = [468767**i+48484*i for i in range (n_val)]
+    profits_vals = []
+    for seed in seeds:
+        r = reservation_prices(I,J,T,beta,b,min_r,max_r,seed)
+        model, variables = linear_model(I,J,T,M,c,r)
+        profits_vals.append(model.ObjVal)
+    if plot :
+        plt.hist(profits_vals,bins=15)
+        plt.xlabel("Profit (fonction objectif)")
+        plt.ylabel("Nombre de résultats")
+        plt.title("Distribution du profit en fonction de la génération aléatoire ")
+
 if expo:       
-    Export_df.to_json("GP30_Results_anthony.json",orient="records",lines=True)
+    Export_df.to_json("GP30_Results.json",orient="records",lines=True)
